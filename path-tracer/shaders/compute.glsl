8a8d5e71557b70ffcf48e6de2515da595f772a3b
Andrew Morton <andrewmorton2@hotmail.co.uk>
2022-09-17 09:09:19 +0100

    Diffuse lambertian working, it seems?

--- path-tracer/shaders/compute.glsl
+++ path-tracer/shaders/compute.glsl
@@ -5,7 +5,7 @@ layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
 layout(rgba32f, binding = 0) uniform image2D screen;
 
 uniform vec3 cameraPosition = vec3(0.0, 0.0, 0.0);
-uniform vec3 directionalLight = vec3(0.0, -1.0, 0.0);
+uniform vec3 directionalLight = vec3(0.5, -1.0, -1.0);
 
 struct Ray
 {
@@ -20,9 +20,7 @@ struct Sphere
 	vec3 color;
 };
 
-uniform Sphere sph = { vec3(0.0, 0.0, -5.0), 2.0, vec3(0.0, 0.0, 0.5) };
-
-
+uniform Sphere sph = { vec3(0.0, 0.0, -1.5), 1.0, vec3(0.0, 0.0, 1.0) };
 
 vec3 trace(struct Ray viewRay)
 {
@@ -35,19 +33,27 @@ vec3 trace(struct Ray viewRay)
 	// if discriminant is positive, there's intersection
 	if (sqrt(b*b - 4*a*c) >= 0.0)
 	{
-		float t = (-b + sqrt(b*b - 4*a*c)) / 2*a;
+		float t1 = (-b - sqrt(b*b - 4*a*c)) / 2*a;
+		float t2 = (-b + sqrt(b*b - 4*a*c)) / 2*a;
 
+		float t = min(t1, t2);
+		//return vec3(t, t, t);
 		// calculate hit point
 		vec3 hitPos = normalize(viewRay.pos + t);
 		//return hitPos;
 		// calclulate normal at hit point
-		vec3 normal = hitPos - sph.center;
+		vec3 normal = (hitPos - sph.center);
 		//return normal;
 
 		//return hitPos;
 
-		float col = dot(normal, -normalize(directionalLight));
+		float col = dot(normal, -directionalLight);
+		//if (col < 0.62)
+		//{
+		//	col = col*5;
+		//}
 		
+		//return sph.color;
 		return sph.color * col;
 		//return vec3(col, col, col);
 	}
@@ -65,8 +71,8 @@ void main()
 	vec2 size = imageSize(screen);
 
 	// calculate x and y components of view ray from pixel co-ord
-	float x = -(float(pixelCoord.x * 2 - size.x) / size.x);
-	float y = -(float(pixelCoord.y * 2 - size.y) / size.y);
+	float x = (float(pixelCoord.x * 2 - size.x) / size.x);
+	float y = (float(pixelCoord.y * 2 - size.y) / size.y);
 
 	struct Ray viewRay;
 	viewRay.pos = vec3(x, y, -1.0);
